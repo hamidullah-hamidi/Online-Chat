2d617b283bd7eabfba3a5b19f3e0a64403fe9ce2
owias azizi <owaisazizi360@gmail.com>
2025-01-20 20:37:10 +0430

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,13 @@ import { useState } from 'react'
 import './styles/App.css'
 import Sidebar from './ui/SideBar';
 import ChatList from './ui/ChatWindow';
-import ChatWindow from './ui/ChatList';
 
 function App() {
    return (
     <div className="app">
-      <Sidebar />
       <div className="main">
+        <Sidebar />
         <ChatList />
-        <ChatWindow />
       </div>
     </div>
   );
