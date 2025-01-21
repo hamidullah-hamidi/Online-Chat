ec794192a44af16a919b79deb3fe29804d8d1254
owias azizi <owaisazizi360@gmail.com>
2025-01-21 21:36:28 +0430

    update

--- src/ui/Login.jsx
+++ src/ui/Login.jsx
@@ -3,29 +3,32 @@ import React, { useState } from 'react';
 import styles from '../styles/Login.css';
 
 const Login = () => {
+
+    const[isShow,setIsShow] = useState(false)
+
     function handleSubmit(){
         console.log('submit');
     }
 
     return (
-        <div className={styles.formContainer}>
+        <div className={`formContainer${isShow ? 'hidden' : '' }`}>
             <h2>Create Acount</h2>
             <form onSubmit={handleSubmit}>
                 <div className='formGroup'>
                     <label htmlFor="name">Name</label>
-                    <input type="text" required/>
+                    <input type="text" name = 'name'required/>
                 </div>
-                <div>
+                <div className='formGroup'>
+                    <label htmlFor="lastName">lastName</label>
+                    <textarea id="lastName" name="lastName" rows="4" required />
+                </div>
+                <div className='formGroup'>
+                    <label htmlFor="phone">Phone Number</label>
+                    <input type="number" id="phone" name="phone" required />
+                </div>
+                <div className='formGroup'>
                     <label htmlFor="email">Email</label>
                     <input type="email" id="email" name="email" required />
-                </div>
-                <div>
-                    <label htmlFor="message">Message</label>
-                    <textarea id="message" name="message" v rows="4" required />
-                </div>
-                <div>
-                    <label htmlFor="phone">Phone Number</label>
-                    <input type="tel" id="phone" name="phone" required />
                 </div>
                 <button type="submit">Submit</button>
             </form>
