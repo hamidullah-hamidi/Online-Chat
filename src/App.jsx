8c5df86ddd29a1bf75577a9a9b0aacd3569b3c1e
owias azizi <owaisazizi360@gmail.com>
2025-01-28 00:24:14 +0700

    some more clarity comments

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Sidebar from './ui/sidebar/SideBar';
 import MessageWindow from './ui/messageWindow/MessageWindow';
 import { CreateUserProvider } from './contexts/CreateUser';
 
+//all sections
 function App() {
    return (
     <div className="app">
