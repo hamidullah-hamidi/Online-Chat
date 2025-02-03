c3a87efbc170688969644de1605bf3e059445267
owias azizi <owaisazizi360@gmail.com>
2025-02-04 00:41:09 +0430

    some comments

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,19 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function App() {
-  const queryClint = new QueryClient({  defaultOptions :
-    {
-    queries:{
-      // cacheTime : 60 * 1000
-      cacheTime : 0
-    }
-  }
-  })
+  // const queryClint = new QueryClient({  defaultOptions :
+  //   {
+  //   queries:{
+  //     // cacheTime : 60 * 1000
+  //     cacheTime : 0
+  //   }
+  // }
+  // })
 
    return (
     <div className="app">
-      <QueryClientProvider client={queryClint}>
-        <ReactQueryDevtools initialIsOpen = {false}/>
+      {/* <QueryClientProvider client={queryClint}> */}
+        {/* <ReactQueryDevtools initialIsOpen = {false}/> */}
         <CreateUserProvider>
         <BrowserRouter>
           <Routes>
@@ -28,7 +28,7 @@ function App() {
           </Routes>
         </BrowserRouter>
         </CreateUserProvider>
-      </QueryClientProvider>
+      {/* </QueryClientProvider> */}
     </div>
   );
 }
