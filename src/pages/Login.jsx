8ac0039d37f573886925c6839ebd47f8d2af042b
Hamidullah <hamidullahh754@gmail.com>
2025-02-05 22:48:04 +0430

    u

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,50 +6,53 @@ import { NavLink } from 'react-router';
 
 const Login = () => {
   const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
   const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
 
   const { setAccounts } = useCreateAccount();
 
   function handleSubmit(e) {
     console.log('submited');
-    
+
     e.preventDefault();
     setAccounts((accounts) => [...accounts, { name, lastName, email }]);
+    setName('');
+    setLastName('');
+    setEmail('');
   }
 
   return (
     <div className='form-container'>
       <NavLink to='appLoyout'>main</NavLink>
-          <form id='addForm' onSubmit={handleSubmit}>
-            <h1 className='form-title'>User Login</h1>
-            <div>
-              <input
-                type='text'
-                placeholder='Name'
-                required
-                onChange={(e) => setName(e.target.value)}
-              />
-              <input
-                type='text'
-                placeholder='Last Name'
-                required
-                onChange={(e) => setLastName(e.target.value)}
-              />
-              <input
-                type='email'
-                placeholder='Email'
-                required
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div className='input-container'>
-              <button type='submit' className='btn-register'>
-                Register
-              </button>
-            </div>
-          </form>
+      <form id='addForm' onSubmit={handleSubmit}>
+        <h1 className='form-title'>User Login</h1>
+        <div>
+          <input
+            type='text'
+            placeholder='Name'
+            required
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type='text'
+            placeholder='Last Name'
+            required
+            onChange={(e) => setLastName(e.target.value)}
+          />
+          <input
+            type='email'
+            placeholder='Email'
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
+        <div className='input-container'>
+          <button type='submit' className='btn-register'>
+            Register
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
