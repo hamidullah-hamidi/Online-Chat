f7c6744940d38e248786c836cd3324330b6ffd48
Hamidullah <hamidullahh754@gmail.com>
2025-02-05 22:55:28 +0430

    i supabase

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,37 +9,21 @@ import { redirect } from 'react-router';
 const Login = () => {
   const [name, setName] = useState('');
   const [lastName, setLastName] = useState('');
-<<<<<<< HEAD
-  const [email, setEmail] = useState('');
-
-  const { setAccounts } = useCreateAccount();
-
-  function handleSubmit(e) {
-    console.log('submited');
-
-    e.preventDefault();
-    setAccounts((accounts) => [...accounts, { name, lastName, email }]);
-    setName('');
-    setLastName('');
-    setEmail('');
-=======
   const [password, setPassword] = useState('');
-  const navigate = useNavigate()
-  
+  const navigate = useNavigate();
+
   // const { setAccounts } = useCreateAccount();
 
   function handleSubmit(e) {
-    e.preventDefault()
-    createUser(name,password,lastName)
-    navigate('/appLoyout')
+    e.preventDefault();
+    createUser(name, password, lastName);
+    navigate('/appLoyout');
     // setAccounts((accounts) => [...accounts, { name, lastName, password }]);
->>>>>>> upstream/main
   }
 
   return (
     <div className='form-container'>
       <NavLink to='appLoyout'>main</NavLink>
-<<<<<<< HEAD
       <form id='addForm' onSubmit={handleSubmit}>
         <h1 className='form-title'>User Login</h1>
         <div>
@@ -56,41 +40,11 @@ const Login = () => {
             onChange={(e) => setLastName(e.target.value)}
           />
           <input
-            type='email'
-            placeholder='Email'
+            type='string'
+            placeholder='password'
             required
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={(e) => setPassword(e.target.value)}
           />
-=======
-          <form id='addForm' onSubmit={handleSubmit}>
-            <h1 className='form-title'>User Login</h1>
-            <div>
-              <input
-                type='text'
-                placeholder='Name'
-                required
-                onChange={(e) => setName(e.target.value)}
-              />
-              <input
-                type='text'
-                placeholder='Last Name'
-                required
-                onChange={(e) => setLastName(e.target.value)}
-              />
-              <input
-                type='string'
-                placeholder='password'
-                required
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-            <div className='input-container'>
-              <button type='submit' className='btn-register'>
-                Register
-              </button>
-            </div>
-          </form>
->>>>>>> upstream/main
         </div>
         <div className='input-container'>
           <button type='submit' className='btn-register'>
