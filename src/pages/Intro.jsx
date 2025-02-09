0e32f2a57764ad4bea3f830339a23130f8def46e
owias azizi <owaisazizi360@gmail.com>
2025-02-09 19:07:54 +0430

    progress

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -1,11 +1,13 @@
 import { NavLink } from "react-router";
+import './intro.css'
 
 export default function Intro(){
     return(
          <div className="intro">
           <h1>Wellcome To Chat App!</h1>
           <p>By using this app you can chat with your friends and share your toughts and ideas to your friends</p>
-          <NavLink to='/login'>Login</NavLink>
-          </div>
+          <NavLink to='/login' className='links'>You have an account?</NavLink>
+          <NavLink to='/login' className='links'>Create Account</NavLink>
+         </div>
           )
 }
