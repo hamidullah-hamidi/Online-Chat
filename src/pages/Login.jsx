b558f8a2dcc77d6e1a4a26fc9a14c547602d9683
Hamidullah Hamidi <hamidullahh754@gmail.com>
2025-02-12 23:06:30 +0430

    u

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,45 +1,33 @@
-
-
 import { useState } from 'react';
 import { NavLink, useNavigate } from 'react-router';
 import { createUser, login } from '../services/apiUsers';
-import './Login.css'
+import './Login.css';
 
 const Login = () => {
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
-<<<<<<< HEAD
+
   const navigate = useNavigate();
 
-  // const { setAccounts } = useCreateAccount();
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    createUser(name, password, lastName);
-    navigate('/appLoyout');
-    // setAccounts((accounts) => [...accounts, { name, lastName, password }]);
-=======
-  const navigate = useNavigate()
-
   async function handleSubmit(e) {
-    e.preventDefault()
-    const isUserExist = await login(name,password)
+    e.preventDefault();
+    const isUserExist = await login(name, password);
     console.log(isUserExist);
-    
-    if(isUserExist){
-      navigate('/appLoyout')
-    }else{
-      alert('Your are not logged in. please create and acount')
+
+    if (isUserExist) {
+      navigate('/appLoyout');
+    } else {
+      alert('Your are not logged in. please create and acount');
     }
->>>>>>> c1416f78be41f27182ee8d8a0c93825cfdc34e53
   }
 
   return (
     <div className='form-container'>
-<<<<<<< HEAD
-      <NavLink to='appLoyout'>main</NavLink>
+      <NavLink to='/appLoyout'>main</NavLink>
+      <NavLink to='/'>intro</NavLink>
+
       <form id='addForm' onSubmit={handleSubmit}>
-        <h1 className='form-title'>User Login</h1>
+        <h1 className='form-title'>Login</h1>
         <div>
           <input
             type='text'
@@ -48,44 +36,11 @@ const Login = () => {
             onChange={(e) => setName(e.target.value)}
           />
           <input
-            type='text'
-            placeholder='Last Name'
-            required
-            onChange={(e) => setLastName(e.target.value)}
-          />
-          <input
             type='string'
             placeholder='password'
             required
             onChange={(e) => setPassword(e.target.value)}
           />
-=======
-      <NavLink to='/appLoyout'>main</NavLink>
-      <NavLink to='/'>intro</NavLink>
-
-          <form id='addForm' onSubmit={handleSubmit}>
-            <h1 className='form-title'>Login</h1>
-            <div>
-              <input
-                type='text'
-                placeholder='Name'
-                required
-                onChange={(e) => setName(e.target.value)}
-              />
-              <input
-                type='string'
-                placeholder='password'
-                required
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-            <div className='input-container'>
-              <button type='submit' className='btn-register'>
-                Register
-              </button>
-            </div>
-          </form>
->>>>>>> c1416f78be41f27182ee8d8a0c93825cfdc34e53
         </div>
         <div className='input-container'>
           <button type='submit' className='btn-register'>
--- src/services/apiUsers.js
+++ src/services/apiUsers.js
@@ -1,7 +1,6 @@
 import supabase from './supabase';
 
 export async function getUsers() {
-<<<<<<< HEAD
   let { data, error } = await supabase.from('users').select('*');
 
   if (error) {
@@ -11,11 +10,11 @@ export async function getUsers() {
   return data;
 }
 
-export async function createUser(name, lastName, password) {
+export async function createUser(name, lastName, password, email) {
   try {
     const { data, error } = await supabase
       .from('users')
-      .insert([{ name, lastName, password }])
+      .insert([{ name, lastName, password, email }])
       .select();
     if (error) {
       console.error('Error inserting data:', error);
@@ -26,61 +25,33 @@ export async function createUser(name, lastName, password) {
     console.log(err);
   }
 }
-=======
-    let { data, error } = await supabase
-        .from('users')
-        .select('*')
 
-    if (error) {
-        throw new Error('users could not be loaded')
-        console.error(error)
-    }
-    return data
+export async function login(name, password) {
+  let { data, error } = await supabase.from('users').select('name,password');
+  if (error) {
+    return null;
+  }
+
+  return data.some((el) => el.name === name && el.password === password);
 }
-
-export async function createUser(name, lastName, password, email) {
-    try {
-        const { data, error } = await supabase.from('users').insert([{ name, lastName, password, email },]).select()
-        if (error) {
-            console.error('Error inserting data:', error);
-            return null;
-        }
-        return data
-    } catch (err) {
-        console.log(err);
-    }
-}
-
-export async function login(name,password) {
-    let { data, error } = await supabase.from('users').select('name,password')
-     if(error){
-        return null
-     }
-     
-     return data.some(el=> el.name === name && el.password === password)
-} 
 
 export async function checkUserEmail(email) {
-    try {
-        let { data: emailArray, error } = await supabase.from('users').select('email')
-        if (error) throw new Error(error)
-         const isExist = emailArray.some(el => el.email === email)
-          return isExist
-
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    let { data: emailArray, error } = await supabase.from('users').select('email');
+    if (error) throw new Error(error);
+    const isExist = emailArray.some((el) => el.email === email);
+    return isExist;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 export async function getUser(id) {
-    try {
-        let { data, error } = await supabase.from('users').eq({id:id}).single()
-        if (error) throw new Error(error)
-          return data
-
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    let { data, error } = await supabase.from('users').eq({ id: id }).single();
+    if (error) throw new Error(error);
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
 }
-
->>>>>>> c1416f78be41f27182ee8d8a0c93825cfdc34e53
